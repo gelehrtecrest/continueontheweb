69e9ff9a938f97d0b5ed96071dce1d99e5b15553
Gelehrte Crest <ein.gelehrte+ff14@gmail.com>
2021-04-17 17:03:56 +0900

    fix

--- docs/main.js
+++ docs/main.js
@@ -5,7 +5,8 @@ function GetTweet(str,code){
 	//var input_data = text_all.value.replace(/\r?\n/g, '%0D%0A');
 
 	var encoded = encodeURI(text_all);
-	var data = url + 'get.html?t=' + encoded;
+	var urldata = url + 'get.html?t=' + encoded;
+	var data = "この続きはWebで " + urldata;
 
 	TWEET.innerHTML =　'<a class="btn btn-primary" href="https://twitter.com/intent/tweet?text=' + data + '" target="_blank">Twitterを起動</a>'
 }
