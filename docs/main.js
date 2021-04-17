3bdcb8951e32d4c911e3d4b0d308e69627ab7e11
Gelehrte Crest <ein.gelehrte+ff14@gmail.com>
2021-04-17 18:10:50 +0900

    fix

--- docs/main.js
+++ docs/main.js
@@ -6,7 +6,7 @@ function GetTweet(str,code){
 	var encoded = btoa(encodeURIComponent(text_all.value))
 	//var encoded = encodeURI(text_all.value);
 	var urldata = url + 'get.html?t=' + encoded;
-	var data = "\n\n #この続きをWebで読もう " + urldata 
+	var data = "この続きをWebで読もう " + urldata
 
 	TWEET.innerHTML =　'<a class="btn btn-primary" href="https://twitter.com/intent/tweet?text=' + data + '" target="_blank">Twitterを起動</a>'
 }
