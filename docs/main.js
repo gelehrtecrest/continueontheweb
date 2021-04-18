19cca3d42ffdbee0f98f5d91682a471a91018de7
Gelehrte Crest <ein.gelehrte+ff14@gmail.com>
2021-04-18 10:39:12 +0900

    文字を読み込むタイミングを修正

--- docs/main.js
+++ docs/main.js
@@ -1,12 +1,19 @@
-const url = "https://gelehrtecrest.github.io/continueontheweb/"
+const url = "https://gelehrtecrest.github.io/continueontheweb/";
+const twitterurl = "https://twitter.com/intent/tweet?text=";
+$(document).ready( function(){
+	$("a").on('click',function(event){
+		event.preventDefault();
+		//var linkUrl = $(this).attr('href');
+		var text_all = document.getElementById("tweetBox");
 
-function GetTweet(str,code){
-	var text_all = document.getElementById("tweetBox");
+		var encoded = btoa(encodeURIComponent(text_all.value))
+		var urldata = url + 'get.html?t=' + encoded;
+		var data = "%0D%0A%0D%0A%23この続きをWebで読もう " + urldata;
+		var linkUrl = twitterurl + data;
 
-	var encoded = btoa(encodeURIComponent(text_all.value))
-	//var encoded = encodeURI(text_all.value);
-	var urldata = url + 'get.html?t=' + encoded;
-	var data = "この続きをWebで読もう " + urldata
-
-	TWEET.innerHTML =　'<a class="btn btn-primary" href="https://twitter.com/intent/tweet?text=' + data + '" target="_blank">Twitterを起動</a>'
-}
+		function action() {
+			location.href = linkUrl;
+		}
+		setTimeout(action,1000);
+	});
+});
