712d06282f1a96ca2d308ba92ac5006c78085bcf
Gelehrte Crest <ein.gelehrte+ff14@gmail.com>
2021-11-26 17:25:13 +0900

    bitlyを使うことに

--- docs/main.js
+++ docs/main.js
@@ -3,7 +3,6 @@ const twitterurl = "https://twitter.com/intent/tweet?text=";
 $(document).ready( function(){
 	$("a").on('click',function(event){
 		event.preventDefault();
-		//var linkUrl = $(this).attr('href');
 		var text_all = document.getElementById("tweetBox");
 
 		var encoded = btoa(encodeURIComponent(text_all.value))
@@ -12,8 +11,32 @@ $(document).ready( function(){
 		var linkUrl = twitterurl + data;
 
 		function action() {
-			location.href = linkUrl;
+			convertBitly(urldata).done(function (d) {
+				if(d.data.url){
+					data = "%0D%0A%0D%0A%23この続きをWebで読もう " + d.data.url;
+					linkUrl = twitterurl + data;
+				}
+				location.href = linkUrl;
+			});
 		}
 		setTimeout(action,1000);
 	});
 });
+
+function convertBitly(url) {
+    var encUrl = encodeURIComponent(url);
+    var bitly = "https://api-ssl.bitly.com/v3/shorten?access_token=73a5adbde17b423b927541b3d13f14fb03310146&longUrl=" + encUrl;
+
+    var d = new $.Deferred();
+
+    $.ajax({
+    	type: "get",
+    	url: bitly,
+    	dataType: "jsonp",
+    	processData: false,
+    	contentType: false,
+    	success: d.resolve,
+    	error: d.reject
+    });
+    return d.promise();
+}
